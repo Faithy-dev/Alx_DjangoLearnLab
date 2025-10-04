da0ace51032b6a426d43e6404842e7f6c7cde04b
Faithy.dev <fathiaolusegun@gmail.com>
2025-10-04 03:14:07 +0100

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,25 +1,34 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from .models import Post
-
-class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ("username", "email", "password1", "password2")
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ("username", "email",)
+from .models import Post, Comment, Tag
 
 class PostForm(forms.ModelForm):
+    tags = forms.CharField(
+        required=False,
+        help_text="Enter tags separated by commas",
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+
     class Meta:
         model = Post
-        fields = ["title", "content"]
+        fields = ['title', 'content', 'tags']
         widgets = {
-            "title": forms.TextInput(attrs={"class": "form-control"}),
-            "content": forms.Textarea(attrs={"class": "form-control", "rows": 8}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
+    def save(self, commit=True, *args, **kwargs):
+        post = super().save(commit=False)
+        if commit:
+            post.save()
+            tags_list = [t.strip() for t in self.cleaned_data['tags'].split(",") if t.strip()]
+            post.tags.clear()
+            for tag_name in tags_list:
+                tag, created = Tag.objects.get_or_create(name=tag_name)
+                post.tags.add(tag)
+        return post
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
